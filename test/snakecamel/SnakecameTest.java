e113f99ed20bc543612ec3f0bf395f68b81e0ed9
g16941ss <g16941ss@s30532.tsuda.ac.jp>
2018-04-26 15:01:54 +0900

    演習課題3-3 エラー修正

--- test/snakecamel/SnakecameTest.java
+++ test/snakecamel/SnakecameTest.java
@@ -1,14 +1,25 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
+import snakecamel.SnakeCamelUtil;
 import org.junit.Test;
+
 
 public class SnakecameTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakecaseからcamelcaseへ変換(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "AbcDefGhi";
+		String actual = scu.snakeToCamelcase("abc_def_ghi");
+		assertThat(actual,is(expected));
 	}
-
+	@Test
+	public void camelcaseからsnakecaseへ変換() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "abc_def_ghi";
+		String actual = scu.camelToSnakecase("AbcDefGhi");
+		assertThat(actual, is(expected));
+	}
 }
